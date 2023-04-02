9204e056a4e3ddbbfb60ed257d5cbc792c15676d
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-02 18:19:26 +0500

    Massive

---
+++ HomeworkMassive/HomeworkMassive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeworkMassive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
