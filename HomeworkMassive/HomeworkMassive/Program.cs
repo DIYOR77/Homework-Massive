f082b99794ea2179b8e3649a78243ad5f3cd8384
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-02 18:22:25 +0500

    First

--- HomeworkMassive/HomeworkMassive/Program.cs
+++ HomeworkMassive/HomeworkMassive/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Console.Write(" Massiv uzunligini kiriting: ");
+            string uz = Console.ReadLine();
+            bool isnumber = int.TryParse(uz, out int son);
+            int[] arr = new int[son];
+            int sanoq = 0;
+            if (isnumber)
+            {
+
+                for (int i = 0; i < arr.Length + 5; i++)
+                {
+                    arr[i] = random.Next(1, 99);
+
+                    if (arr[i] % 2 == 1)
+                    {
+                        sanoq++;
+                        Console.Write(" " + arr[i]);
+                        if (sanoq == son) break;
+                    }
+                    else i = 0;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Faqat butun son kirita olas")
+            }
+            Main(args);
         }
     }
 }
