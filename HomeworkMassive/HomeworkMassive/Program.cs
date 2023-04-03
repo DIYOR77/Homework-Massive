22f1692f2466aecca003bc3f24d0a28cba65f5ea
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-03 14:54:37 +0500

    Seventh

--- HomeworkMassive/HomeworkMassive/Program.cs
+++ HomeworkMassive/HomeworkMassive/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Console.Write(" Massiv uzunligini kiriting: ");
+            string son=Console.ReadLine();
+            bool isnumber=int.TryParse(son, out int number);
+            int[]arr=new int[number];
+            if (isnumber)
+            {
+                for(int i=0; i < number; i++)
+                {
+                    arr[i]=random.Next(1,99);
+                    Console.Write(" "+arr[i]);
+                }
+                Console.WriteLine();
+                Array.Sort(arr);
+                for(int i=0; i < number; i++)
+                {
+                    Console.Write(" " + arr[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Faqat butun son kirita olasiz: ");
+            }
+            
         }
     }
 }
